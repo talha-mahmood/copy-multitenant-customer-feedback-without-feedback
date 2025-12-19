3d053855d3e5db25489ab35fdedfdd2c80daa52e
talha-mahmood <talhamahmood1261@gmail.com>
2025-12-19 10:34:06 +0500

    working: placed userid field in customers and merchants, removed extra migration files

--- src/database/migrations/1747380858728-create_notifications_table.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class CreateDevelopmentAmenitiesNotificationsTable1747380858728
-  implements MigrationInterface
-{
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
--- src/modules/customers/entities/customer.entity.ts
+++ src/modules/customers/entities/customer.entity.ts
@@ -1,9 +1,17 @@
-import { Column, Entity, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany, ManyToOne, JoinColumn } from 'typeorm';
 import { Exclude } from 'class-transformer';
 import { BaseEntity } from 'src/common/entities/base.entity';
+import { User } from 'src/modules/users/entities/user.entity';
 
 @Entity('customers')
 export class Customer extends BaseEntity {
+  @Column({ name: 'user_id', nullable: true })
+  user_id: number;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
   @Column({ length: 100 }) name: string;
   @Column({ unique: true }) email: string;
   @Column({ unique: true }) phone: string;
