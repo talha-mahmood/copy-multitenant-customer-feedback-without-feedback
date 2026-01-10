79b33749275a5039e201f6dd70b39eb64e3b5051
talha-mahmood <talhamahmood1261@gmail.com>
2026-01-10 15:08:16 +0500

    updated credit package dto

--- src/modules/wallets/dto/update-credit-package.dto.ts
+++ src/modules/wallets/dto/update-credit-package.dto.ts
@@ -1,55 +1,5 @@
 import { IsString, IsInt, IsNumber, IsOptional, IsIn, Min, IsBoolean } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCreditPackageDto } from './create-credit-package.dto';
 
-export class UpdateCreditPackageDto {
-  @IsString()
-  @IsOptional()
-  name?: string;
-
-  @IsString()
-  @IsOptional()
-  description?: string;
-
-  @IsInt()
-  @Min(1)
-  @IsOptional()
-  credits?: number;
-
-  @IsString()
-  @IsIn(['marketing', 'utility', 'general'])
-  @IsOptional()
-  credit_type?: string;
-
-  @IsNumber()
-  @Min(0)
-  @IsOptional()
-  price?: number;
-
-  @IsNumber()
-  @Min(0)
-  @IsOptional()
-  price_per_credit?: number;
-
-  @IsString()
-  @IsOptional()
-  currency?: string;
-
-  @IsString()
-  @IsOptional()
-  @IsIn(['annual', 'temporary', 'all'])
-  merchant_type?: string;
-
-  @IsInt()
-  admin_id: number;
-
-  @IsBoolean()
-  @IsOptional()
-  is_active?: boolean;
-
-  @IsInt()
-  @IsOptional()
-  sort_order?: number;
-
-  @IsInt()
-  @IsOptional()
-  bonus_credits?: number;
-}
+export class UpdateCreditPackageDto extends PartialType(CreateCreditPackageDto) {}
