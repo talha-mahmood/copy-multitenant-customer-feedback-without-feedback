18b7f61d44caabfc482e4744efe71c27f3013333
FaaizMir <faaizmir123@gmail.com>
2026-01-13 19:30:32 +0500

    agent expiring done , merchant expiring and status changed , not done

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -9,11 +9,12 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { NestjsFormDataModule } from 'nestjs-form-data';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserHasRoleModule } from '../roles-permission-management/user-has-role/user-has-role.module';
-import { EncryptionHelper } from '../../common/helpers/encryption-helper';  
+import { EncryptionHelper } from '../../common/helpers/encryption-helper';
 import { UserModule } from '../users/user.module';
 import { adminProviders } from '../admins/admin.provider';
 import { merchantProviders } from '../merchants/merchant.provider';
 import { customerProviders } from '../customers/customer.provider';
+import { walletProviders } from '../wallets/wallet.provider';
 
 @Module({
   imports: [
@@ -38,9 +39,10 @@ import { customerProviders } from '../customers/customer.provider';
     ...adminProviders,
     ...merchantProviders,
     ...customerProviders,
+    ...walletProviders,
     IsUniqueConstraint,
     EncryptionHelper,
   ],
   exports: [AuthService, JwtModule, IsUniqueConstraint, EncryptionHelper],
 })
-export class AuthModule {}
+export class AuthModule { }
